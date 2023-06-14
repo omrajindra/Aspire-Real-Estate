91eb05e17bdaf3ea887eb5158bac494365e6061c
omrajindra <omrajindra.ori@gmail.com>
2023-06-14 17:56:26 +0545

    Backend finish

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const authController = require("./controllers/authController");
+const propertyController = require("./controllers/propertyController");
+const uploadController = require("./controllers/uploadController");
 const app = express();
 
 // mongodb connect
@@ -9,6 +12,14 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, () =>
   console.log("MongoDB has been started success")
 );
+
+// routes and middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authController);
+app.use("/property", propertyController);
+app.use("/upload", uploadController);
 
 // starting server
 app.listen(process.env.PORT, () =>
