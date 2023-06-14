c4b5fe11a2c3ba42d1b2ba2edde55556785df9ed
omrajindra <108399386+omrajindra@users.noreply.github.com>
2023-06-14 13:08:06 +0000

    Navbar half created

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,44 @@
 
 import './App.css';
+import FeaturedProperties from './components/featuredProperties';
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from './components/navbar/Navbar';
+import Newsletter from './components/newsletter/Newsletter';
+import PopularProperties from "./components/popularProperties";
+import Properties from "./components/properties/Properties";
+import PropertyDetail from "./components/propertyDetail/PropertyDetail";
+import Signin from "./components/signin/Signin";
+import Signup from "./components/signup/Signup";
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<navbar/>} />
-        <Route path='/' element={} />
-        <Route path='/' element={} />
-        <Route path='/' element={} />
-        <Route path='/' element={} />
+        <Route path='/' element={
+          <>
+      <Navbar/>
+      <Hero/>  
+      <PopularProperties/>
+      <FeaturedProperties/>
+      <Newsletter/>
+      <Footer/>
+      </>
+      } />
+        
+        <Route path='/properties' element={<>
+        <Navbar/>
+        <Properties/>
+        <Footer/>
+        </>} />
+        <Route path='/propertyDetail/:id' element={<>
+        <Navbar/>
+        <Properties/>
+        <Footer/>
+        </>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/signin' element={<Signin />} />
       </Routes>
     </div>
   );
